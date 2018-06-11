4aef208cf77ea412ebe984c9ed6b13895a95e34b
Shun Nishimura <n.shun1110@gmail.com>
2018-06-11 18:26:22 +0900

    :heavy_plus_sign: viterbi algorithm

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 let argv = require('optimist').argv;
 
 (async function main() {
-	if (!argv.kind || (argv.kind !== 'local' && argv.kind !== 'global'))
+	if (!argv.kind || (argv.kind !== 'local' && argv.kind !== 'global' && argv.kind !== 'hmm'))
 		return console.log('no "kind" option!');
 
-	const aligment = argv.kind === 'local' ?
-		require('./local/local-alignment') : require('./global/global-alignment');
-	let result = await aligment(argv);
+	const main = require(`./${argv.kind}/main`);
+	let result = await main(argv);
 
 	console.log(JSON.stringify(result, null, 1));
 })();
